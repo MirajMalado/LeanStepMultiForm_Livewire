eef16a10b373b0797f11753e845522accb543359
MirajMalado <diallomalado8@gmail.com>
2022-04-04 16:25:17 +0000

    Livewire Component

--- Livewire/resources/views/register.blade.php
+++ Livewire/resources/views/register.blade.php
@@ -6,8 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enregistrement</title>
     <link rel="stylesheet" href="{{ asset('bootstrap.min.css') }}">
+    @livewireStyles
 </head>
 <body>
-    hello
+    
+    <div class="container">
+        <div class="row" style="margin-top:50px">
+        <div class="col-md-6 offset-md-3">
+            <h1> Multi Step Form</h1><hr>
+
+        </div>
+
+        </div>
+
+    </div>
+
+    @livewireScripts
 </body>
 </html>
